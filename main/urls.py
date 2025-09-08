a2e87de4cf3b93b38cdbc18d8de1a8a439070e45
ariyan <ariyanbolandi3@gmail.com>
2025-09-08 15:04:56 +0330

    feat(feed serializer): add serializer to feed Viewset

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,8 @@ router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 
 router.register('profile', views.UserProfileViewSet)
 
+router.register('feed', views.UserProfileFeedViewSet)
+
 urlpatterns = [
     path('hello-view/', views.HelloAPIView.as_view()),
     path('login/', views.UserLoginApiView.as_view()),
